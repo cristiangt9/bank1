973e4a997fd86d0e1d2e4ac2ceb5d9d0509daf72
Cristian Gonzalez <cristiangt9@gmail.com>
2021-04-03 19:31:02 -0500

    Cleaning function deposit_for_nonexisting_account_return_an_account_created of innecesary comments

--- tests/Feature/EventControllerTest.php
+++ tests/Feature/EventControllerTest.php
@@ -15,16 +15,11 @@ class EventControllerTest extends TestCase
     public function deposit_for_nonexisting_account_return_an_account_created()
     {
         // teniendo una solicitud post al enpoint 'api/event' con el metodo post
-        // $response = $this->post('api/event', [
-        //     \GuzzleHttp\RequestOptions::JSON => ["type" => "deposit", "destination" => "100", "amount" => 10]
-        // ]);
         $response = $this->post('api/event', ["type" => "deposit", "destination" => "100", "amount" => 10
         ]);
         // cuando el enpoint responde
         // entonces obtendremos un status 201, un content: 'ok' y la base de datos limpia
         $response->assertStatus(201);
         $response->assertJsonFragment(["destination" => ["id" => "100", "balance" => 10]]);
-        // $this->assertEquals($response->getContent(), "OK");
-        // $this->assertCount(0, Account::all());
     }
 }
