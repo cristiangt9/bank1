ae9da29b6c9cbaeb82f99c07bfa79a4595d8d080
Cristian Gonzalez <cristiangt9@gmail.com>
2021-04-04 15:47:20 -0500

    Add test for the case balance_for_existing_account_return_balance_only

--- tests/Feature/BalanceControllerTest.php
+++ tests/Feature/BalanceControllerTest.php
@@ -22,9 +22,25 @@ class BalanceControllerTest extends TestCase
         // Cuando solicitemos un balance (withdraw) de una cuenta no existente
         // Entonces el endpoint nos respondera con una respuesta con content: 0 y codigo http 404
 
-        $responce = $this->get('api/bance?account_id=1234' );
+        $responce = $this->get('api/balance?account_id=1234' );
         $responce->assertStatus(404);
         $this->assertEquals($responce->getcontent(), '0');
     }
-    
+    /** @test  */
+    public function balance_for_existing_account_return_balance_only()
+    {
+        // # Get balance for existing account
+        // GET /balance?account_id=100
+        // 200 20
+        // Teniendo una solicitud al endpoint 'api/balance' con el metodo GET, y una cuenta "100" con un bancele de 20
+        // Cuando solicitemos un balance (withdraw) de una cuenta existente
+        // Entonces el endpoint nos respondera con una respuesta con content: 20 y codigo http 404
+        $this->post('api/event', [
+            "type" => "deposit", "destination" => "100", "amount" => 20
+        ]);
+        $responce = $this->get('api/balance?account_id=100' );
+        $responce->assertStatus(200);
+        $this->assertEquals($responce->getcontent(), 20);
+    }
+
 }
